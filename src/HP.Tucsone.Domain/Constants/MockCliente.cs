b29cf4021e6b55bb94455e9f239a844e804be7ef
Sebastian Capurro <sebacapu3@gmail.com>
2025-03-23 03:08:27 -0300

    coreccion de categorias

--- src/HP.Tucsone.Domain/Constants/MockCliente.cs
+++ src/HP.Tucsone.Domain/Constants/MockCliente.cs
@@ -5,9 +5,9 @@
         public static List<Cliente> GetClientesMock()
         {
             var categoriaClassic = MockCategorias.categorias.Where(c => c.GetNombre().ToLower() == "classic").First();
-            var categoriaGold = MockCategorias.categorias.Where(c => c.GetNombre().ToLower() == "classic").First();
-            var categoriaDiamond = MockCategorias.categorias.Where(c => c.GetNombre().ToLower() == "classic").First();
-            var categoriaPlatinum = MockCategorias.categorias.Where(c => c.GetNombre().ToLower() == "classic").First();
+            var categoriaGold = MockCategorias.categorias.Where(c => c.GetNombre().ToLower() == "gold").First();
+            var categoriaDiamond = MockCategorias.categorias.Where(c => c.GetNombre().ToLower() == "diamond").First();
+            var categoriaPlatinum = MockCategorias.categorias.Where(c => c.GetNombre().ToLower() == "platinum").First();
 
             return new List<Cliente>()
             {
