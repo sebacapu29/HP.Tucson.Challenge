4390579f3fab48bd65bead316362233956233d6b
Sebastian Capurro <sebacapu3@gmail.com>
2025-03-24 20:40:53 -0300

    completo lógica de handler para cliente en espera

--- src/HP.Tucsone.Application/Mesa/Handlers/LiberarMesaCommandHandler.cs
+++ src/HP.Tucsone.Application/Mesa/Handlers/LiberarMesaCommandHandler.cs
@@ -1,14 +1,48 @@
 ﻿using HP.Tucsone.Application.Mesa.Models.Commands;
 using HP.Tucsone.Application.Mesa.Models.Responses;
+using HP.Tucsone.Application.Services.Interfaces;
+using HP.Tucsone.Domain.Interfaces;
 using MediatR;
 
 namespace HP.Tucsone.Application.Mesa.Handlers
 {
     public class LiberarMesaCommandHandler : IRequestHandler<LiberarMesaCommand, LiberarMesaResponse>
     {
-        public Task<LiberarMesaResponse> Handle(LiberarMesaCommand request, CancellationToken cancellationToken)
+        private readonly IMesaRepository _mesaRepository;
+        private readonly IClienteEnEsperaService _clienteEnEsperaService;
+        private readonly IReservaRepository _reservaRepository;
+        private readonly IClienteRepository _clienteRepository;
+        public LiberarMesaCommandHandler(IMesaRepository mesaRepository,
+                                         IClienteEnEsperaService clienteEnEsperaService,
+                                         IReservaRepository reservaRepository,
+                                         IClienteRepository clienteRepository)
         {
-            throw new NotImplementedException();
+            _mesaRepository = mesaRepository;
+            _clienteEnEsperaService = clienteEnEsperaService;
+            _reservaRepository = reservaRepository;
+            _clienteRepository = clienteRepository;
+        }
+        public async Task<LiberarMesaResponse> Handle(LiberarMesaCommand request, CancellationToken cancellationToken)
+        {
+            var mesa = _mesaRepository.ObtenerMesaPorNumero(request.Numero);
+            var fechaHora = DateTime.Now;
+            if(mesa == null)
+            {
+                throw new Exception("No se encontró la mesa especificada");
+            }
+            _mesaRepository.LiberarMesa(mesa);
+            var clientesEnEspera = await _clienteEnEsperaService.ObtenerClientesEnEspera();
+            if (clientesEnEspera != null && clientesEnEspera.Any())
+            {
+                var ultimoClienteEnEspera = clientesEnEspera.Last();
+                var categoriaCliente = await _clienteRepository.GetCategoriaCliente(ultimoClienteEnEspera.Categoria);
+                var clienteReserva = new Domain.Cliente(ultimoClienteEnEspera!.NumeroCliente, ultimoClienteEnEspera.Nombre, categoriaCliente);
+                var idNuevaReserva = await _reservaRepository.GenerarId();
+                var reservaDeClienteEneEspera = new Domain.Reserva(idNuevaReserva, fechaHora, clienteReserva, mesa.Numero);
+                await _reservaRepository.CrearReserva(reservaDeClienteEneEspera);
+                return new LiberarMesaResponse { Mensaje = $"Mesa liberada y se asignó al cliente en espera {ultimoClienteEnEspera.Nombre}" };
+            }
+            return new LiberarMesaResponse { Mensaje = "Mesa liberada" };
         }
     }
 }
