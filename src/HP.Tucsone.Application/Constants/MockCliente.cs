8fe2f11f2c8a34d5e5a43b40e6a5ddb9e53c7be0
Sebastian Capurro <sebacapu3@gmail.com>
2025-03-25 18:46:01 -0300

    muevo directorio a otro proyecto

---
+++ src/HP.Tucsone.Application/Constants/MockCliente.cs
@@ -0,0 +1,21 @@
+﻿namespace HP.Tucsone.Application.Constants
+{
+    public static class MockCliente
+    {
+        public static List<Domain.Cliente> GetClientesMock()
+        {
+            var categoriaClassic = MockCategorias.categorias.Where(c => c.Nombre.ToLower() == "classic").First();
+            var categoriaGold = MockCategorias.categorias.Where(c => c.Nombre.ToLower() == "gold").First();
+            var categoriaDiamond = MockCategorias.categorias.Where(c => c.Nombre.ToLower() == "diamond").First();
+            var categoriaPlatinum = MockCategorias.categorias.Where(c => c.Nombre.ToLower() == "platinum").First();
+
+            return new List<Domain.Cliente>()
+            {
+                new Domain.Cliente(1,"Jose", categoriaClassic),
+                new Domain.Cliente(2,"Juan", categoriaGold),
+                new Domain.Cliente(3,"Pedro", categoriaDiamond),
+                new Domain.Cliente(4,"Esteban", categoriaPlatinum),
+            };
+        }
+    }
+}
---
+++ src/HP.Tucsone.Application/Constants/MockMesa.cs
@@ -0,0 +1,25 @@
+﻿namespace HP.Tucsone.Application.Constants
+{
+    public static class MockMesa
+    {
+        public static List<Domain.Mesa> GetMesas()
+        {
+            var listaMesas = new List<Domain.Mesa>();
+            for (int i = 1; i <= 40; i++) { 
+                if( i <= 18)
+                {
+                    listaMesas.Add(new Domain.Mesa(i, 2));
+                }
+                else if( i > 18 && i <= 33)
+                {
+                    listaMesas.Add(new Domain.Mesa(i,4));
+                }
+                else
+                {
+                    listaMesas.Add(new Domain.Mesa(i, 6));
+                }
+            }
+            return listaMesas;  
+        }
+    }
+}
